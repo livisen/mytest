f8a7fbded7fada102057d00830704d962d23bcab
Fenfen Wan <fenfen.wan@autodesk.com>
2019-12-12 10:01:17 +0800

    test may have confict 2

---
+++ wff-mayconflict - Copy.sql
@@ -0,0 +1,7 @@
+version https://git-lfs.github.com/spec/v1
+<<<<<<< Updated upstream
+oid sha256:12b1af2edcc26a0b9e9218995d0cb50c9fd7cc6981d08f8e25f81a3be415c132
+=======
+oid sha256:13e36f2710b5c01f9f0c799ddbc804248cd3b25c65e466836a0bc82287c2cdbe
+>>>>>>> Stashed changes
+size 291328
